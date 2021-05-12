4a047c4db2acbfdab19aeda061a2ae4d3ee90c4c
maxe23 <maxeiteljorg@gmail.com>
2021-05-11 21:53:14 -0400

    added event handler function

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -11,3 +11,6 @@ var form = d3.select("form")
 // Create event handlers 
 button.on("click", runEnter)
 form.on("submit",runEnter)
+
+// Complete the event handler function for the form
+function runEnter() {
