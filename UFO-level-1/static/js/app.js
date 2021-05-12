cf2533a2ce637131b7ec78d6cd7fcf34c48b4b86
maxe23 <maxeiteljorg@gmail.com>
2021-05-11 21:55:28 -0400

    added element

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -17,6 +17,9 @@ function runEnter() {
 
       // Prevent the page from refreshing
       d3.event.preventDefault()
-      
+
       // Select the input element and get the raw HTML node
     var inputElement = d3.select("#datetime")
+
+      // Get the value property of the input element
+      var inputValue = inputElement.property("value")
